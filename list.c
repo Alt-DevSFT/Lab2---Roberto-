4c7e841078cef5e76ff4be18a8e9b17d614b2dc6
Your Name <you@example.com>
2026-03-27 01:55:15 +0000

    new try

--- list.c
+++ list.c
@@ -51,7 +51,6 @@ List * createList() {
 void * firstList(List * list) {
 
     list->current = list->head;
-    if(list->current) return list->current->(*int)data;
 }
 
 void * nextList(List * list) {
